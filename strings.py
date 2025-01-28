923e6d9d3bc52e3978bf76589fa98eee2d359a42
milicablagojevicc <milicab48@gmail.com>
2025-01-28 16:47:18 +0100

    simple strings commit

---
+++ strings.py
@@ -0,0 +1,12 @@
+#Simple Strings
+s1 = "John" #double quote
+s2 = 'Mary' #single quote
+print(s1)
+print(s2)
+
+s1 = "And then he asked: 'how are you?'"
+print(s1)
+s2 = "And then she asked: 'how are you?'"
+print(s2)
+s1 = "this is how to have \"and\" inside a string" # \" = escape
+print(s1)
